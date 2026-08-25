bfb54ea8c4fedd7e8b7accfaa24ee911055400a0
axolotl <user@example.com>
2026-08-25 14:02:09 -0400

    {Added timer loop and time remaining reminders}

--- Timer.java
+++ Timer.java
@@ -13,6 +13,12 @@ public class Timer {
 		String task = getter();
 
 		print("Your timer for working on " + task + " starts now!");
+
+		for (int minutes = 15; minutes > 0; minutes--) {
+			if (minutes % 5 == 0) {
+				System.out.println(minutes + " minutes remaining for the task " + task);
+			}
+		}
 	}
 
 	public void print(String text) {
