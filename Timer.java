830964ed283429bc4ce94b9681a3a7b52cea94b4
axolotl <user@example.com>
2026-08-25 13:43:36 -0400

    {Setup Timer class}

---
+++ Timer.java
@@ -0,0 +1,15 @@
+public class Timer {
+	private String taskName;
+
+	public Timer(String taskName) {
+		this.taskName = taskName;
+	}
+
+	public String getter() {
+		return taskName;
+	}
+
+	public void timer() {
+	
+	}
+}
