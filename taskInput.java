56af1c1521108d28af8a4188300b2ba5488c9b7d
axolotl <dte.biotech46@gmail.com>
2026-09-05 05:39:29 -0400

    {Finished task loop}

--- taskInput.java
+++ taskInput.java
@@ -50,23 +50,49 @@ public class taskInput {
 		boolean continueInput = true;
 
 		do {
+			// get input
+
 			Scanner input = new Scanner(System.in);
 			
 			print("\nEnter a task you need to work on (type \"STOP\" when you've put in all your tasks): ");
 
 			String task = input.nextLine();
 
+			// add task as JSON key-value
+
+			this.json += "\t\"" + id + "\": \"" + task + "\",";
+
+			// check if user is done entering tasks
+
 			String taskUpperCase = task.toUpperCase();
 
 			int taskIsSTOP = taskUpperCase.compareTo("STOP");
 
 			if (taskIsSTOP == 0) {
+				
 				continueInput = false;
 
 				print("\nTask input loop has stopped, proceeding to startup the workflow timer...");
+			
+			
 			}
+		
+			this.id += 1; // increase id number for each task
 
 		} while (continueInput == true);
+
+		this.json += "}";
+	}
+
+	/**
+	 * Name: writeTasks()
+	 * Purpose: overwrite contents of tasks.jsonc and add new set of tasks
+	 * @param none
+	 * @return none
+	 */
+
+	public static void writeTasks() {
+		
 	}
 
 	public static void print(String text) {
